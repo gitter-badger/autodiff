97ebc5766e9855b4ab393e6032fd07be7891a18d
carl297r <carl+github@livermore.tc>
2014-08-26 15:30:55 -0400

    Initial

---
+++ src/main/scala/AutoDiff.scala
@@ -0,0 +1,11 @@
+/**
+ * Created by clivermore on 8/8/14.
+ *
+ */
+object AutoDiff extends App {
+
+  val y = math.sin(90)
+  println(y)
+}
+
+
---
+++ src/main/scala/BreezeQuickStart.scala
@@ -0,0 +1,36 @@
+/**
+ * Created by clivermore on 8/12/14.
+ *
+ */
+
+import breeze.linalg._
+
+object BreezeQuickStart extends App {
+
+  val x = DenseVector.zeros[Double](5)
+
+  x(2) = 2
+  x(3 to 4) := .5
+  x(0 to 1) := DenseVector(.1,.2)
+
+  for {
+    i <- 0 until x.length
+  } x(i) = i
+
+  val y = x.t
+  //println(y)
+
+  val z = x * y
+
+  println (x)
+  println(y)
+
+
+  //println(z)
+
+
+  ///println(z * x)
+
+  //println(y * z)
+
+}
